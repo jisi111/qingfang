001c38a9260426c46682bf2e269d1ca3afc56e6e
zaijianliu <39648397+jisi111@users.noreply.github.com>
2019-11-14 14:08:45 +0800

    Add files via upload

---
+++ script/constant.js
@@ -0,0 +1,17 @@
+hostIp = 'http://120.77.213.175:8002';
+// hostIp = 'http://192.168.3.16:8002';
+// hostIp = 'http://192.168.0.103:8002';
+
+shareIp = 'http://120.77.213.175:8005';
+
+socketIp = 'ws://120.77.213.175:8002';
+// socketIp = 'ws://192.168.3.16:8002';
+// socketIp = 'ws://192.168.0.103:8002';
+
+// 声网
+agoraAppId = '6054fd315838456cb6ba97091b0ccd8c';// 测试
+
+// 阿里云图片上传配置
+aliBucketName = "chuansi";
+aliEndPoint = "http://oss-cn-hangzhou.aliyuncs.com";
+imageDomain = "https://chuansi.oss-cn-hangzhou.aliyuncs.com";// 图片下载地址
